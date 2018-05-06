37b42857eb2eb081588f03bb7dfb9d276af8c57b
Brian Kung <brian@callmekung.com>
2018-05-06 09:03:30 -0500

    Moves stuff into day.js

--- app/javascript/packs/days.js
+++ app/javascript/packs/days.js
@@ -1,5 +1,23 @@
 import debounce from 'lodash-es/debounce'
 
+const dateParam = window.location.toString().match(/\d{4}-\d{2}-\d{2}/)[0]
+const dateFormat = 'YYYY-MM-DD';
+const date = moment(dateParam);
+
+const changeDate = (targetDate) => {
+  window.location = window.location.toString().replace(dateParam, targetDate)
+}
+
+const back = (timeUnit) => {
+  const newDate = date.subtract(1, timeUnit).format(dateFormat)
+  return changeDate(newDate)
+}
+
+const forward = (timeUnit) => {
+  const newDate = date.add(1, timeUnit).format(dateFormat)
+  return changeDate(newDate)
+}
+
 $(document).ready(function() {
   $('#dones').on('input', debounce(
     () => {
@@ -17,4 +35,20 @@ $(document).ready(function() {
     },
     500
   ))
+
+  $('.search-bar input').on('keydown', (e) => {
+    // 37 == left arrow, 39 == right arrow
+    const { metaKey, keyCode, altKey, shiftKey } = e
+    const leftArrow = keyCode == 37
+    const rightArrow = keyCode == 39
+
+    if (metaKey && leftArrow)   return back('year')
+    if (metaKey && rightArrow)  return forward('year')
+    if (altKey && leftArrow)    return back('month')
+    if (altKey && rightArrow)   return forward('month')
+    if (shiftKey && leftArrow)  return back('week')
+    if (shiftKey && rightArrow) return forward('week')
+    if (leftArrow)              return back('day')
+    if (rightArrow)             return forward('day')
+  });
 })
--- app/javascript/packs/dones.js
+++ app/javascript/packs/dones.js
@@ -1,27 +1,3 @@
-const paramPattern = /q=([^?&]+)/i;
-const params = window.location.search;
-// const paramValue = paramPattern.exec(params)[1];
-const dateParam = window.location.toString().match(/\d{4}-\d{2}-\d{2}/)[0]
-const dateFormat = 'YYYY-MM-DD';
-const date = moment(dateParam);
-const { addTime: add, subtractTime: subtract } = date
-const paramIsDate = Boolean(date);
-const paramDate = date.format(dateFormat);
-
-const changeDate = (targetDate) => {
-  window.location = window.location.toString().replace(dateParam, targetDate)
-}
-
-const back = (timeUnit) => {
-  const newDate = date.subtract(1, timeUnit).format(dateFormat)
-  return changeDate(newDate)
-}
-
-const forward = (timeUnit) => {
-  const newDate = date.add(1, timeUnit).format(dateFormat)
-  return changeDate(newDate)
-}
-
 $(document).ready(() => {
   const date = $('#done_created_at').val();
   const dateText = window.localStorage.getItem(date);
@@ -47,22 +23,4 @@ $(document).ready(() => {
   // This stuff was on dones/index.html.erb
 
   $('.search-bar input').focus();
-
-  if (paramIsDate) {
-    $('.search-bar input').on('keydown', (e) => {
-      // 37 == left arrow, 39 == right arrow
-      const { metaKey, keyCode, altKey, shiftKey } = e
-      const leftArrow = keyCode == 37
-      const rightArrow = keyCode == 39
-
-      if (metaKey && leftArrow)   return back('year')
-      if (metaKey && rightArrow)  return forward('year')
-      if (altKey && leftArrow)    return back('month')
-      if (altKey && rightArrow)   return forward('month')
-      if (shiftKey && leftArrow)  return back('week')
-      if (shiftKey && rightArrow) return forward('week')
-      if (leftArrow)              return back('day')
-      if (rightArrow)             return forward('day')
-    });
-  }
 });
