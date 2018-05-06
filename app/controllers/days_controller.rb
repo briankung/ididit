87487fff9d5536eee853ab6067422b5416d06577
Brian Kung <brian@callmekung.com>
2018-05-06 08:17:16 -0500

    Clamps date to YYYY-MM-DD values

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,5 +1,24 @@
 class DaysController < ApplicationController
+  before_action :clamp_date
+
   def edit
     @dones = Done.where(date: params[:date]).pluck(:text).join("\n")
+  end
+
+  private
+
+  def stupid_american_date
+    @match = params[:date].match /^(?<MM>\d{2})-(?<DD>\d{2})-(?<YYYY>\d{4})$/
+  end
+
+  def reformatted_date
+    @match.values_at(:YYYY, :MM, :DD).join('-')
+  end
+
+  def clamp_date
+    return redirect_to day_edit_path(reformatted_date) if stupid_american_date
+    Date.strptime(params[:date], "%Y-%m-%d")
+  rescue ArgumentError
+    redirect_to day_edit_path(Date.current)
   end
 end
