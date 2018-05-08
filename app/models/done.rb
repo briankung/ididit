385c9f58c6a70fef49bc75c8d681cd69dd3c5419
Brian Kung <brian@callmekung.com>
2018-05-08 13:26:43 -0500

    Adds equality check

--- app/models/done.rb
+++ app/models/done.rb
@@ -3,4 +3,10 @@ class Done < ApplicationRecord
   scope :today, -> { where date: Date.today }
   scope :on_date, ->(date) { where date: date }
   scope :text, -> { pluck :text }
+
+  def == other
+    [self, other].map(&:attributes)
+                 .map {|attrs| attrs.values_at('text', 'date')}
+                 .reduce(:==)
+  end
 end
