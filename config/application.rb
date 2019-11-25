6291505d86b4903ff3dccb7997efb35020150585
Brian Kung <github@briankung.xyz>
2019-11-25 08:12:25 -0600

    Add snippet to pick up local timezone

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,12 @@ module IDidIt
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Central Time (US & Canada)'
+
+    # Disable sudo password on macos:
+    # https://apple.stackexchange.com/a/333055/335512
+    # basically `sudo visudo` then add the line `briankung ALL=(ALL) NOPASSWD: ALL`
+    # Undo after return from China
+    # config.time_zone = `sudo systemsetup -gettimezone`.chomp.split.last
     config.active_record.default_timezone = :local
     config.generators do |g|
       g.test_framework  nil, fixture: false
