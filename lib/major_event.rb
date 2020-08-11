208f72be0b92437882dbe8ffc8f5e28db0e785e0
Brian Kung <github@briankung.xyz>
2020-08-11 17:00:40 -0500

    Handle edge cases with dates being DateTimes

--- lib/major_event.rb
+++ lib/major_event.rb
@@ -6,7 +6,7 @@ class MajorEvent
   include ActionView::Helpers::NumberHelper
 
   def initialize(display_text, event_date)
-    @display_text, @event_date = display_text, event_date
+    @display_text, @event_date = display_text, event_date.to_date
   end
 
   def to_s current_date
@@ -22,7 +22,13 @@ class MajorEvent
   def display_date current_date
     dotiw = dotiw(@event_date, current_date, false, except: %i{hours minutes seconds})
 
-    dotiw = "-#{dotiw}" if current_date < @event_date
+    dotiw = if @event_date.to_date === current_date.to_date
+              "today"
+            elsif current_date < @event_date
+              "in #{dotiw}"
+            else
+              "#{dotiw} ago"
+            end
 
     "#{@display_text}: #{dotiw}"
   end
